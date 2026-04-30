fb2a5bc4c7f248f76f12b71df7eab093c08852bd
MiguelAS2104 <ms1941400@gmail.com>
2026-04-29 20:30:07 -0600

    volviendo todo a como era al principio xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import { useState } from 'react'
-import { Routes, Route } from "react-router-dom"
-
-import Home from "./pages/Home"
-import Sensorial from "./pages/Sensorial"
-import Cocteles from "./pages/Cocteles"
-
-import "./App.css"
+import './App.css'
 
 const App = () => {
+
   return (
-    <Routes>
-
-      <Route path="/" element={<Home />} />
-
-      <Route path="/sensorial" element={<Sensorial />} />
-
-      <Route path="/cocteles" element={<Cocteles />} />
-
-    </Routes>
+    <>
+      <h1>Componente principal</h1>
+    </>
   )
 }
 
-export default App
+export default App;
