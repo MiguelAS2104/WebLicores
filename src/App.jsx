57188f9b123a898e5bbf1f0533302b789ec9b759
MiguelAS2104 <ms1941400@gmail.com>
2026-04-29 20:17:47 -0600

    eliminar linea del _redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { userState } from 'react'
-import './App.css'
+import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Sensorial from "./pages/Sensorial"
+import Cocteles from "./pages/Cocteles"
+
+import "./App.css"
 
 const App = () => {
-
   return (
-    <>
-      <h1>Componente principal</h1>
-    </>
+    <Routes>
+
+      <Route path="/" element={<Home />} />
+
+      <Route path="/sensorial" element={<Sensorial />} />
+
+      <Route path="/cocteles" element={<Cocteles />} />
+
+    </Routes>
   )
 }
 
-export default App;
+export default App
